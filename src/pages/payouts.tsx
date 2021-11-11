681893de0d3aac5bb8478416899c4de3a0c47b9d
affanashraf120 <affanashraf120@gmail.com>
2021-11-11 13:48:58 +0500

    Padding Fixed

--- src/pages/payouts.tsx
+++ src/pages/payouts.tsx
@@ -28,80 +28,82 @@ export default wrapDashboardLayout(function PayoutsPage() {
 				<title>Payouts - Camelot</title>
 			</Head>
 
-			<div className="py-12 font-poppins">
-				<div className="text-2xl mb-3">Hi {userState.user!.firstName},</div>
-				<div className="text-5xl font-semibold">Payouts 💰</div>
-			</div>
+			<div style={{ maxWidth: '1400px', margin: '0 auto' }}>
+				<div className="py-12 font-poppins">
+					<div className="text-2xl mb-3">Hi {userState.user!.firstName},</div>
+					<div className="text-5xl font-semibold">Payouts 💰</div>
+				</div>
 
-			<div className="grid lg:grid-cols-3">
-				<div className="col-span-2">
-					<div className="-card --shadow p-8 h-full">
-						<PaymentMethod></PaymentMethod>
+				<div className="grid lg:grid-cols-3">
+					<div className="col-span-2">
+						<div className="-card --shadow p-8 h-full">
+							<PaymentMethod></PaymentMethod>
+						</div>
+					</div>
+					<div className="flex flex-col items-center justify-center text-center">
+						<div className="text-xl font-bold">Your account credits</div>
+						<div className="text-primary text-7xl my-10 font-bold">
+							{!transactionsData ? (
+								<div className="flex items-center justify-center">
+									<Spinner></Spinner>
+								</div>
+							) : (
+								'$' + transactionsData.currentCredits
+							)}
+						</div>
+						<button
+							className="-button -primary"
+							disabled={!transactionsData?.currentCredits}
+						>
+							Withdraw All Credits
+						</button>
 					</div>
 				</div>
-				<div className="flex flex-col items-center justify-center text-center">
-					<div className="text-xl font-bold">Your account credits</div>
-					<div className="text-primary text-7xl my-10 font-bold">
-						{!transactionsData ? (
-							<div className="flex items-center justify-center">
-								<Spinner></Spinner>
-							</div>
-						) : (
-							'$' + transactionsData.currentCredits
-						)}
+
+				<hr className="my-10" />
+
+				<div className="-card --shadow">
+					<div className="-table-responsive">
+						<table className="-table">
+							<thead>
+								<tr>
+									<th>Transaction</th>
+									<th>Status</th>
+									<th>Date Processed</th>
+									<th>Fees</th>
+								</tr>
+							</thead>
+							<tbody>
+								{transactionsData?.transactions.map(tr => {
+									return (
+										<tr>
+											<td>
+												<div className="flex flex-row items-center font-bold">
+													?
+												</div>
+											</td>
+											<td>Tuesday, 07:22 PM</td>
+											<td>
+												{dayjs(tr.createdAt).format('DD MMM YYYY, HH:mm A')}
+											</td>
+											<td>
+												<div className="flex flex-row items-center text-green-500">
+													<Square
+														className="bg-green-500 mr-2"
+														length="1.2rem"
+													></Square>
+													Completed
+												</div>
+											</td>
+											<td className="text-blue-600">
+												${parseInt(tr.delta) / 100 || '?'}
+											</td>
+										</tr>
+									)
+								})}
+							</tbody>
+						</table>
 					</div>
-					<button
-						className="-button -primary"
-						disabled={!transactionsData?.currentCredits}
-					>
-						Withdraw All Credits
-					</button>
-				</div>
-			</div>
-
-			<hr className="my-10" />
-
-			<div className="-card --shadow">
-				<div className="-table-responsive">
-					<table className="-table">
-						<thead>
-							<tr>
-								<th>Transaction</th>
-								<th>Status</th>
-								<th>Date Processed</th>
-								<th>Fees</th>
-							</tr>
-						</thead>
-						<tbody>
-							{transactionsData?.transactions.map(tr => {
-								return (
-									<tr>
-										<td>
-											<div className="flex flex-row items-center font-bold">
-												?
-											</div>
-										</td>
-										<td>Tuesday, 07:22 PM</td>
-										<td>
-											{dayjs(tr.createdAt).format('DD MMM YYYY, HH:mm A')}
-										</td>
-										<td>
-											<div className="flex flex-row items-center text-green-500">
-												<Square
-													className="bg-green-500 mr-2"
-													length="1.2rem"
-												></Square>
-												Completed
-											</div>
-										</td>
-										<td className="text-blue-600">
-											${parseInt(tr.delta) / 100 || '?'}
-										</td>
-									</tr>
-								)
-							})}
-						</tbody>
-					</table>
 				</div>
 			</div>
 		</>
