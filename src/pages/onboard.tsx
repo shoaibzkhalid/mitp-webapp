08dbadf50365d076fe1da4d40a0331ee3acb86be
affanashraf <affanashraf120@gmail.com>
2022-01-28 22:05:32 +0500

    fix landing page

--- src/pages/onboard.tsx
+++ src/pages/onboard.tsx
@@ -12,32 +12,33 @@ import { observer } from 'mobx-react-lite'
 
 export default observer(function OnboardPage() {
 	const router = useRouter()
-	useEffect(() => {
-		// declare the data fetching function
-		const fetchData = async () => {
-			if(!userState.user) return;
-			const potData = await Api.userPots.list()
-			if(potData.length===0) {
-				const state ={
-					step: 0,
-					title: 'Workout for at least 10 minutes',
-					description: '',
-					checkinCount: 1,
-					minAmount: 0
-				}
-				const pot = await Api.userPots.create(state)
+	// useEffect(() => {
+	// 	// declare the data fetching function
+	// 	const fetchData = async () => {
+	// 		if(!userState.user) return;
+	// 		const potData = await Api.userPots.list()
+	// 		if(potData.length===0) {
+	// 			const state ={
+	// 				step: 0,
+	// 				title: 'Workout for at least 10 minutes',
+	// 				description: '',
+	// 				checkinCount: 1,
+	// 				minAmount: 0
+	// 			}
+	// 			const pot = await Api.userPots.create(state)
 
-				runInAction(() => {
-					selectedPotState.moneyPotId = pot?.id as string
-				})
-			} else {
-				runInAction(() => {
-					selectedPotState.moneyPotId = potData[0]?.moneyPotId as string
-				})
-			}
-		}
-		fetchData()
-	  }, [userState])
+	// 			runInAction(() => {
+	// 				selectedPotState.moneyPotId = pot?.id as string
+	// 			})
+	// 		} else {
+	// 			runInAction(() => {
+	// 				selectedPotState.moneyPotId = potData[0]?.moneyPotId as string
+	// 			})
+	// 		}
+	// 	}
+	// 	fetchData()
+	//   }, [userState])
+	console.log('asdf', userState.user)
 	if (!userState.user) {
 		return <SpinnerBig />
 	} else {
